ba95753da374afb04e595430b43fc4868242d904
Sven Willrodt <9309822+Thamsta@users.noreply.github.com>
2023-11-13 21:43:27 +0100

    Add time logging.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::collections::HashMap;
+use std::time::Instant;
 
 use ndarray::{arr1, Array1, Array2, Axis, s};
 use rand::Rng;
@@ -69,6 +70,7 @@ struct SimulationResponse {
 
 #[tauri::command]
 fn create_rg<'a>(marking: Vec<i32>, transition_inputs: Vec<Vec<i32>>, transition_outputs: Vec<Vec<i32>>) -> Result<RGResponse, String> {
+    let start_time = Instant::now();
     let t = &transition_inputs.len(); // rows
     let p = &transition_inputs.get(0).expect("empty array").len(); // columns
 
@@ -103,14 +105,19 @@ fn create_rg<'a>(marking: Vec<i32>, transition_inputs: Vec<Vec<i32>>, transition
         edges.insert(id, next);
     }
 
-    println!("{:?} states", all_states_rev.keys().len());
+    let end_time = Instant::now();
+    let elapsed_time = end_time - start_time;
+
+    let total_states = all_states_rev.keys().len();
+    let states_per_second = total_states as f64 / elapsed_time.as_secs_f64() / 1000f64;
+
+    println!("RG with {:?} states took {}ms ({}k states/s)", total_states, elapsed_time.as_millis(), states_per_second.round());
 
     return Ok(RGResponse { success: true });
 }
 
 fn insert_next_state(new_state: Array1<i32>, all_states: &mut Vec<Array1<i32>>, all_states_rev: &mut HashMap<Array1<i32>, u32>, queue: &mut Vec<Array1<i32>>) -> Option<u32> {
     let existing = all_states_rev.get(&new_state);
-    println!("Checking {:?}, is already existing: {}: {:?}", new_state, all_states_rev.contains_key(&new_state), existing);
     match existing {
         None => {
             all_states_rev.insert(new_state.clone(), all_states.len() as u32);
