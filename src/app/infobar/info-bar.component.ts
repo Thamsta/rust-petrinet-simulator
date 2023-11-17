e0eda4914ca019b80ec606b862472d3caf80396e
Sven Willrodt <9309822+Thamsta@users.noreply.github.com>
2023-11-17 15:35:47 +0100

    Format infobar

--- src/app/infobar/info-bar.component.ts
+++ src/app/infobar/info-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RGResponse} from "../models";
 
 @Component({
@@ -7,14 +7,24 @@ import {RGResponse} from "../models";
   styleUrls: ['./info-bar.component.scss']
 })
 export class InfoBarComponent {
-    states: number = 0;
-    edges: number = 0;
+    states: string = "0";
+    edges: string = "0";
 
     isReversible: boolean = false
 
     public updateRGInfos(infos: RGResponse) {
         console.log("updating with", infos)
-        this.states = infos.states
-        this.edges = infos.edges
+        this.states = this.formatNumber(infos.states)
+        this.edges = this.formatNumber(infos.edges)
+    }
+
+    private formatNumber(num: number): string {
+        // Use scientific notation for numbers larger than 10,000,000
+        if (Math.abs(num) >= 1e7) {
+            return num.toExponential(1);
+        }
+
+        // Format the number with spaces every three digits
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
     }
 }
