e2cc07af91628598865f4c7bfccf13c45ec257f0
Sven Willrodt <9309822+Thamsta@users.noreply.github.com>
2023-11-29 01:09:17 +0100

    Improve performance of liveness check.

--- src-tauri/src/reachability.rs
+++ src-tauri/src/reachability.rs
@@ -1,16 +1,16 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::time::Instant;
 
 use ndarray::{arr1, Array1, Array2};
 use petgraph::{Direction, Graph};
 use petgraph::algo::tarjan_scc;
 use petgraph::graph::{DiGraph, NodeIndex};
-use petgraph::visit::{EdgeRef, IntoNodeIdentifiers};
+use petgraph::visit::IntoNodeIdentifiers;
 
 use crate::common::*;
 
 pub fn create_rg<'a>(marking: Vec<i32>, transition_inputs: Vec<Vec<i32>>, transition_outputs: Vec<Vec<i32>>) -> Result<RGResponse, String> {
-    let start_time = Instant::now();
+    let start_time_rg = Instant::now();
     let t = &transition_inputs.len(); // rows
     let p = &transition_inputs.get(0).expect("empty array").len(); // columns
 
@@ -47,40 +47,47 @@ pub fn create_rg<'a>(marking: Vec<i32>, transition_inputs: Vec<Vec<i32>>, transi
         step_counter += 1;
     }
 
-    let end_time = Instant::now();
-    let elapsed_time = end_time - start_time;
+    let end_time_rg = Instant::now();
+    let elapsed_time_rg = end_time_rg - start_time_rg;
 
     let total_states = graph.node_count();
     let total_edges = graph.edge_count();
-    let elements_per_second = (total_states + total_edges) as f64 / elapsed_time.as_secs_f64() / 1000f64;
+    let elements_per_second = (total_states + total_edges) as f64 / elapsed_time_rg.as_secs_f64() / 1000f64;
 
-    println!("RG with {:?} states and {} edges took {}ms ({}k elem/s)", total_states, total_edges, elapsed_time.as_millis(), elements_per_second.round());
+    println!("RG with {:?} states and {} edges took {}ms ({}k elem/s)", total_states, total_edges, elapsed_time_rg.as_millis(), elements_per_second.round());
 
+    let start_time_properties = Instant::now();
     let properties = check_properties(&graph, *t);
+    let end_time_properties = Instant::now();
+    let elapsed_time_properties = end_time_properties - start_time_properties;
 
-    println!("Properties: {:?}", properties);
+    println!("Determining properties took {}ms ({:?})", elapsed_time_properties.as_millis(), properties);
 
     return Ok(RGResponse::new(graph.node_count(), graph.edge_count(), properties.reversible, properties.liveness, true, "".to_string()));
 }
 
 fn check_properties(rg: &DiGraph<Array1<i32>, i32>, transitions: usize) -> RgProperties {
     let sccs = tarjan_scc(rg);
-    let scc_graph = create_scc_graph(&sccs, rg);
+    let start_time_properties = Instant::now();
+    let scc_graph = create_scc_graph(&sccs, rg, transitions);
+    let end_time_properties = Instant::now();
+    let elapsed_time_properties = end_time_properties - start_time_properties;
+    println!("SCC-Graph with {} states and {} edges took {}ms", scc_graph.node_count(), scc_graph.edge_count(), elapsed_time_properties.as_millis());
+
 
     let reversible = sccs.len() == 1 && rg.edge_count() > 0;
-    let liveness = check_liveness(&scc_graph, transitions);
+    let liveness = check_liveness(&scc_graph);
 
     return RgProperties { liveness, reversible };
 }
 
-fn check_liveness(scc_graph: &DiGraph<Vec<i32>, ()>, transitions: usize) -> bool {
+fn check_liveness(scc_graph: &DiGraph<bool, ()>) -> bool {
     let mut liveness = true;
     scc_graph.node_identifiers().for_each(|inx| {
         let outgoing_edges = scc_graph.edges_directed(inx, Direction::Outgoing);
         if outgoing_edges.count() == 0 {
-            // this is a terminal SCC
-            let active_transitions = scc_graph.node_weight(inx).unwrap();
-            if active_transitions.len() < transitions {
+            // this is a terminal SCC, check if it is labeled with true
+            if !scc_graph.node_weight(inx).unwrap() {
                 liveness = false;
             }
         };
@@ -88,46 +95,45 @@ fn check_liveness(scc_graph: &DiGraph<Vec<i32>, ()>, transitions: usize) -> bool
     return liveness;
 }
 
-fn create_scc_graph(sccs: &Vec<Vec<NodeIndex>>, rg: &DiGraph<Array1<i32>, i32>) -> DiGraph<Vec<i32>, ()> {
-    let mut graph = DiGraph::<Vec<i32>, ()>::new();
-    let mut nodes = Vec::new(); // acts as a map from index to node
+fn create_scc_graph(sccs: &Vec<Vec<NodeIndex>>, rg: &DiGraph<Array1<i32>, i32>, transitions: usize) -> DiGraph<bool, ()> {
+    let mut graph = DiGraph::<bool, ()>::new();
+    let mut node_to_scc: Vec<usize> = vec![0; rg.node_count()];
 
-    // add SCC nodes upfront so that all nodes already exist.
-    sccs.iter().for_each(| _ | {
-        nodes.push(graph.add_node(Vec::new()))
-    });
-
-    for current_scc in 0..sccs.len() {
-        let scc = sccs.get(current_scc).unwrap();
-        let node = nodes.get(current_scc).unwrap();
-        scc.iter().for_each(|idx| {
-            rg.edges(*idx).for_each(|edge| {
-                // for each edge starting from within the SCC:
-                // if its target is
-                let active_transitions: &mut Vec<i32> = graph.node_weight_mut(*node).unwrap();
-                if scc.contains(&edge.target()) && !active_transitions.contains(&edge.weight()) {
-                    // ... within the SCC, add the weight to active transitions
-                    active_transitions.push(*edge.weight());
-                } else {
-                    // ... not within the SCC, add an edge from this SCC to the target's
-                    let target_scc = get_scc_of_node(&edge.target(), sccs);
-                    graph.add_edge(*nodes.get(current_scc).unwrap(), *nodes.get(target_scc).unwrap(), ());
-                }
-            });
-        })
-    };
-
-    return graph;
-}
-
-fn get_scc_of_node(node: &NodeIndex, sccs: &Vec<Vec<NodeIndex>>) -> usize {
-    for i in 0..sccs.len() {
-        if sccs.get(i).unwrap().contains(node) {
-            return i;
+    for (scc_index, scc) in sccs.iter().enumerate() {
+        for &node_index in scc {
+            node_to_scc[node_index.index()] = scc_index;
         }
     }
 
-    panic!("Tried to find node index {:?} which is not in the SCC graph {:?}", node, sccs)
+    for _ in 0..sccs.len() {
+        graph.add_node(false);
+    }
+
+    // HashSet to store unique edges
+    let mut unique_edges: HashSet<(usize, usize)> = HashSet::new();
+    let mut scc_transitions: HashMap<usize, HashSet<i32>> = HashMap::new();
+
+    for edge in rg.edge_indices() {
+        let (source, target) = rg.edge_endpoints(edge).unwrap();
+        let source_scc = node_to_scc[source.index()];
+        let target_scc = node_to_scc[target.index()];
+        if source_scc != target_scc && unique_edges.insert((source_scc, target_scc)) {
+            graph.add_edge(NodeIndex::new(source_scc), NodeIndex::new(target_scc), ());
+        } else if source_scc == target_scc {
+            let entry = scc_transitions.entry(source_scc).or_insert_with(HashSet::new);
+            entry.insert(rg.edge_weight(edge).unwrap().clone());
+        }
+    }
+
+    // Label nodes based on the presence of internal edges with each label
+    for (_, node) in graph.node_indices().enumerate() {
+        match scc_transitions.get(&(node.index())) {
+            Some(labels) => { graph[node] = labels.len() == transitions; }
+            None => {}
+        }
+    }
+
+    return graph;
 }
 
 fn insert_next_state(old_state_idx: NodeIndex, new_state: Array1<i32>, all_states_rev: &mut HashMap<Array1<i32>, NodeIndex>, graph: &mut Graph<Array1<i32>, i32>, inx: i32, queue: &mut Vec<NodeIndex>) -> Option<NodeIndex> {
