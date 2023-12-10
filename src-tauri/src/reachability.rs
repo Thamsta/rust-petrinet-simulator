34452c59c5eadb0bf03db5f854408fc7c61986ce
Sven Willrodt <9309822+Thamsta@users.noreply.github.com>
2023-12-10 12:43:09 +0100

    Move property checking into own file.

--- src-tauri/src/reachability.rs
+++ src-tauri/src/reachability.rs
@@ -1,13 +1,14 @@
-use std::collections::{HashMap, HashSet};
+mod properties;
+
+use std::collections::HashMap;
 use std::time::Instant;
 
 use ndarray::{arr1, Array1, Array2};
-use petgraph::{Direction, Graph};
-use petgraph::algo::tarjan_scc;
+use petgraph::Graph;
 use petgraph::graph::{DiGraph, NodeIndex};
-use petgraph::visit::IntoNodeIdentifiers;
 
 use crate::common::*;
+use crate::reachability::properties::check_properties;
 
 pub fn create_rg<'a>(marking: Vec<i32>, transition_inputs: Vec<Vec<i32>>, transition_outputs: Vec<Vec<i32>>) -> Result<RGResponse, String> {
     let start_time_rg = Instant::now();
@@ -64,76 +65,6 @@ pub fn create_rg<'a>(marking: Vec<i32>, transition_inputs: Vec<Vec<i32>>, transi
     println!("Determining properties took {}ms ({:?})", elapsed_time_properties.as_millis(), properties);
 
     return Ok(RGResponse::new(graph.node_count(), graph.edge_count(), properties.reversible, properties.liveness, true, "".to_string()));
-}
-
-fn check_properties(rg: &DiGraph<Array1<i32>, i32>, transitions: usize) -> RgProperties {
-    let sccs = tarjan_scc(rg);
-    let start_time_properties = Instant::now();
-    let scc_graph = create_scc_graph(&sccs, rg, transitions);
-    let end_time_properties = Instant::now();
-    let elapsed_time_properties = end_time_properties - start_time_properties;
-    println!("SCC-Graph with {} states and {} edges took {}ms", scc_graph.node_count(), scc_graph.edge_count(), elapsed_time_properties.as_millis());
-
-
-    let reversible = sccs.len() == 1 && rg.edge_count() > 0;
-    let liveness = check_liveness(&scc_graph);
-
-    return RgProperties { liveness, reversible };
-}
-
-fn check_liveness(scc_graph: &DiGraph<bool, ()>) -> bool {
-    let mut liveness = true;
-    scc_graph.node_identifiers().for_each(|inx| {
-        let outgoing_edges = scc_graph.edges_directed(inx, Direction::Outgoing);
-        if outgoing_edges.count() == 0 {
-            // this is a terminal SCC, check if it is labeled with true
-            if !scc_graph.node_weight(inx).unwrap() {
-                liveness = false;
-            }
-        };
-    });
-    return liveness;
-}
-
-fn create_scc_graph(sccs: &Vec<Vec<NodeIndex>>, rg: &DiGraph<Array1<i32>, i32>, transitions: usize) -> DiGraph<bool, ()> {
-    let mut graph = DiGraph::<bool, ()>::new();
-    let mut node_to_scc: Vec<usize> = vec![0; rg.node_count()];
-
-    for (scc_index, scc) in sccs.iter().enumerate() {
-        for &node_index in scc {
-            node_to_scc[node_index.index()] = scc_index;
-        }
-    }
-
-    for _ in 0..sccs.len() {
-        graph.add_node(false);
-    }
-
-    // HashSet to store unique edges
-    let mut unique_edges: HashSet<(usize, usize)> = HashSet::new();
-    let mut scc_transitions: HashMap<usize, HashSet<i32>> = HashMap::new();
-
-    for edge in rg.edge_indices() {
-        let (source, target) = rg.edge_endpoints(edge).unwrap();
-        let source_scc = node_to_scc[source.index()];
-        let target_scc = node_to_scc[target.index()];
-        if source_scc != target_scc && unique_edges.insert((source_scc, target_scc)) {
-            graph.add_edge(NodeIndex::new(source_scc), NodeIndex::new(target_scc), ());
-        } else if source_scc == target_scc {
-            let entry = scc_transitions.entry(source_scc).or_insert_with(HashSet::new);
-            entry.insert(*rg.edge_weight(edge).unwrap());
-        }
-    }
-
-    // Label nodes based on the presence of internal edges with each label
-    for (_, node) in graph.node_indices().enumerate() {
-        match scc_transitions.get(&(node.index())) {
-            Some(labels) => { graph[node] = labels.len() == transitions; }
-            None => {}
-        }
-    }
-
-    return graph;
 }
 
 fn insert_next_state(old_state_idx: NodeIndex, new_state: Array1<i32>, all_states_rev: &mut HashMap<Array1<i32>, NodeIndex>, graph: &mut Graph<Array1<i32>, i32>, inx: i32, queue: &mut Vec<NodeIndex>) -> Option<NodeIndex> {
