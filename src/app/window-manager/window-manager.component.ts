b28db0bbbd8bae436d3bcdb80dffd59a50815ea3
Sven Willrodt <9309822+Thamsta@users.noreply.github.com>
2024-03-04 13:59:33 +0100

    Remove testing code.

--- src/app/window-manager/window-manager.component.ts
+++ src/app/window-manager/window-manager.component.ts
@@ -4,45 +4,14 @@ import {v4 as uuidv4} from "uuid";
 import {FormControl} from "@angular/forms";
 
 @Component({
-  selector: 'app-window-manager',
-  templateUrl: './window-manager.component.html',
-  styleUrls: ['./window-manager.component.scss']
+    selector: 'app-window-manager',
+    templateUrl: './window-manager.component.html',
+    styleUrls: ['./window-manager.component.scss']
 })
 export class WindowManagerComponent {
     selected = new FormControl(0);
     openWindows: OpenWindow[] = [
-        {type: WindowTypes.net, name: "My Net", net: this.sampleNetDTO(), rg: undefined, rgId: ""},
-        {type: WindowTypes.net, name: "My Net 2", net: this.sampleNetDTO(), rg: undefined, rgId: ""},
-        {type: WindowTypes.rg, name: "My Net RG", net: undefined, rg: "digraph {\n" +
-                "    0 [ label = \"[2, 0, 0]\" style=\"bold,filled\" color=\"#7E57C2\" fillcolor=\"#F5F5F5\"]\n" +
-                "    1 [ label = \"[1, 0, 1]\"]\n" +
-                "    2 [ label = \"[1, 1, 0]\"]\n" +
-                "    3 [ label = \"[0, 1, 1]\"]\n" +
-                "    4 [ label = \"[0, 2, 0]\"]\n" +
-                "    5 [ label = \"[0, 0, 2]\"]\n" +
-                "    0 -> 1 [ label = \"0\"]\n" +
-                "    0 -> 2 [ label = \"2\"]\n" +
-                "    2 -> 3 [ label = \"0\"]\n" +
-                "    2 -> 4 [ label = \"2\"]\n" +
-                "    3 -> 4 [ label = \"1\"]\n" +
-                "    1 -> 5 [ label = \"0\"]\n" +
-                "    1 -> 2 [ label = \"1\"]\n" +
-                "    1 -> 3 [ label = \"2\"]\n" +
-                "    5 -> 3 [ label = \"1\"]\n" +
-                "}\n",
-        rgId: ""},
-        {type: WindowTypes.rg, name: "My Net RG", net: undefined, rg: "digraph {\n" +
-                "    0 [ label = \"[hello world]\"]\n" +
-                "    1 [ label = \"[1, 0, 1]\"]\n" +
-                "    2 [ label = \"[1, 1, 0]\"]\n" +
-                "    3 [ label = \"[0, 1, 1]\"]\n" +
-                "    0 -> 1 [ label = \"0\"]\n" +
-                "    0 -> 2 [ label = \"2\"]\n" +
-                "    2 -> 3 [ label = \"0\"]\n" +
-                "    1 -> 2 [ label = \"1\"]\n" +
-                "    1 -> 3 [ label = \"2\"]\n" +
-                "}\n",
-            rgId: ""},
+        {type: WindowTypes.net, name: "My Net", net: this.sampleNetDTO(), rg: undefined, rgId: ""}
     ]
 
     openNewNet(net: NetDTO | undefined) {
