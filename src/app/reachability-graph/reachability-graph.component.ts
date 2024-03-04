c2d6fde382e2cf442f9d9144b2581c58b74d0204
Sven Willrodt <9309822+Thamsta@users.noreply.github.com>
2024-03-04 21:17:49 +0100

    Prototype RG svg export

--- src/app/reachability-graph/reachability-graph.component.ts
+++ src/app/reachability-graph/reachability-graph.component.ts
@@ -2,6 +2,8 @@ import {AfterViewInit, Component, Input} from '@angular/core';
 import {graphviz} from "d3-graphviz";
 import {v4 as uuidv4} from "uuid";
 import {BaseToolbarComponent} from "../base-toolbar/base-toolbar.component";
+import {save} from "@tauri-apps/api/dialog";
+import {writeTextFile} from "@tauri-apps/api/fs";
 
 @Component({
   selector: 'app-reachability-graph',
@@ -22,5 +24,18 @@ export class ReachabilityGraphComponent implements AfterViewInit {
             .width(window.innerWidth)
             .height(window.innerHeight - 52 - BaseToolbarComponent.height) // leave 50 px for the tab bar
             .renderDot(this.graph)
+    }
+
+    async saveSvgAsFile() {
+        let svgContent = document.getElementById(this.id)!.outerHTML
+
+        let filePath = await save({
+            title: "Save Net",
+            filters: [{name: "", extensions: ["svg"]}],
+        });
+        if (filePath == null) return;
+
+
+        await writeTextFile(filePath, svgContent);
     }
 }
