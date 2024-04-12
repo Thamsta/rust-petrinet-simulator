7bbdd6bf5d9dbd477f68fa0389a67db0b1824e53
Sven Willrodt <noreply>
2024-04-12 14:05:31 +0200

    Delete old infobar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDividerModule} from "@angular/material/divider";
-import {InfoBarComponent} from './infobar/info-bar.component';
 import {ShortcutDirective} from './directives/shortcut.directive';
 import {MatTooltipModule} from "@angular/material/tooltip";
 import {ExportComponent} from './export/export.component';
@@ -34,7 +33,6 @@ import {EditorOpenRgDialogComponent} from './editor-open-rg-dialog/editor-open-r
     declarations: [
         AppComponent,
         CanvasComponent,
-        InfoBarComponent,
         ShortcutDirective,
         ExportComponent,
         ImportComponent,
